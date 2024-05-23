ee7903a13649e70df72bb892ebcb17e84520b4f4
Vladimir <v.milashevsky@gmail.com>
2024-05-23 12:29:56 +0300

    refactor: reafactor structure of Movie Stat Panel

--- src/components/MovieStatsPanel/index.tsx
+++ src/components/MovieStatsPanel/index.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import cn from "classnames";
 import {
   BUDGET_SUBTITLE,
   DURATION_SUBTITLE,
@@ -30,20 +29,28 @@ const MovieStatsPanel: FC<MovieStatsPanelProps> = ({
 }) => {
   const displayedGenres = genres.map((item) => item.name).join(", ");
   return (
-    <div className={classes.stats_container}>
-      <div className={cn(classes.info_title, classes.content_list)}>
-        <p>{DURATION_SUBTITLE}</p>
-        <p>{PREMIERE_SUBTITLE}</p>
-        <p>{BUDGET_SUBTITLE}</p>
-        <p>{REVENUE_SUBTITLE}</p>
-        <p>{GENRES_SUBTITLE}</p>
-      </div>
-      <div className={classes.content_list}>
-        <p>{formatTime(runtime)}</p>
-        <p>{formatDate(release_date)}</p>
-        <p>{formatMoney(budget)}</p>
-        <p>{formatMoney(revenue)}</p>
-        <p className="long_text_container">{displayedGenres}</p>
+    <div>
+      <div className={classes.stats_container}>
+        <div className={classes.content_list}>
+          <p className={classes.info_title}>{DURATION_SUBTITLE}</p>
+          <p className="long_text_container">{formatTime(runtime)}</p>
+        </div>
+        <div className={classes.content_list}>
+          <p className={classes.info_title}>{PREMIERE_SUBTITLE}</p>
+          <p className="long_text_container">{formatDate(release_date)}</p>
+        </div>
+        <div className={classes.content_list}>
+          <p className={classes.info_title}>{BUDGET_SUBTITLE}</p>
+          <p className={classes.info_meaning}>{formatMoney(budget)}</p>
+        </div>
+        <div className={classes.content_list}>
+          <p className={classes.info_title}>{REVENUE_SUBTITLE}</p>
+          <p className={classes.info_meaning}>{formatMoney(revenue)}</p>
+        </div>
+        <div className={classes.content_list}>
+          <p className={classes.info_title}>{GENRES_SUBTITLE}</p>
+          <p className="long_text_container">{displayedGenres}</p>
+        </div>
       </div>
     </div>
   );
